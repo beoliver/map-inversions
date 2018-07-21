d889400a879a49eb565cfd9651d2ef09dc07a81b
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-07-21 22:46:51 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject beoliver/map-inversions "1.0.0"
+  :description "simple functions for working with maps"
+  :url "http://github.com/beoliver/map-inversions"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ src/map_inversions/core.clj
@@ -0,0 +1,1 @@
+(ns map-inversions.core)
